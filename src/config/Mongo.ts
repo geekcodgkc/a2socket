ea0d0e5733dd0aaa2da930123e4a6ebdcc157668
william <w3webservices.net@gmail.com>
2023-08-23 12:18:13 -0400

    upgrade in mongodb

--- src/config/Mongo.ts
+++ src/config/Mongo.ts
@@ -2,7 +2,12 @@ import { connect } from "mongoose";
 import { DB_URI } from "./index";
 
 const dbConnect = async (): Promise<void> => {
-	await connect(`${DB_URI}`);
+	try {
+		await connect(`${DB_URI}`);
+		console.log('db connected succesfully')
+	} catch (error) {
+		console.error(error)
+	}
 };
 
 export default dbConnect;
