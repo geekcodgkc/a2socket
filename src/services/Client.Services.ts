3ccf005c4507f367c69830a43a037f802097b166
william <w3webservices.net@gmail.com>
2023-08-24 12:02:32 -0400

    added client route

---
+++ src/services/Client.Services.ts
@@ -0,0 +1,25 @@
+import ClientModel from "../models/Clients.Model";
+
+const CreateCLient = async () => {
+    try {
+        console.log('running')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const DeleteClient = async () => {
+    try {
+        console.log('running')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const UpdateClient = async () => {
+    try {
+        console.log('running')
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/services/Message.Services.ts
+++ src/services/Message.Services.ts
@@ -1,6 +1,10 @@
 import { io } from "../app";
 import MessageModel from "../models/Message.Model";
 
+/*
+	envia mensajes para actualizar la cola atravez del socket
+	y guarda el mensaje en la cola esperando que
+*/
 const sendMessage = async (
 	roomId: string,
 	message: string | object | Array<string | object>,
@@ -8,10 +12,19 @@ const sendMessage = async (
 	io.to(roomId).emit("updateData", message);
 };
 
+/*
+	solicita a los branch que recopilen los datos
+	y sean enviado al mainBranch
+*/
 const salesSevice = async (roomId:string) => {
 	io.to(roomId).emit('syncSales')
 }
 
+/*
+	envia los datos de ventas de los branch
+	para el mainBranch
+	al room que corresponde al branch especifico
+*/
 const sendSalesServices = async (roomId:string, 
 	message: string | object | Array<string | object>) => {
 	io.to(roomId).emit('giveSales', message)
