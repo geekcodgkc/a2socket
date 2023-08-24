3e6f05d7435b4d9f017ca1e0cf186b832828a3b7
william <w3webservices.net@gmail.com>
2023-08-24 12:08:43 -0400

    new service added

--- src/services/Client.Services.ts
+++ src/services/Client.Services.ts
@@ -23,3 +23,36 @@ const UpdateClient = async () => {
         console.log(error)
     }
 }
+
+const GetClient = async () => {
+    try {
+        console.log('running')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const GetClients = async () => {
+    try {
+        console.log('running')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const ValidateClient = async () => {
+    try {
+        console.log('running')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    GetClient,
+    GetClients,
+    UpdateClient,
+    DeleteClient,
+    CreateCLient,
+    ValidateClient,
+}
