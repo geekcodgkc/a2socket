e032f81b13a662366fa41a32c7075fe31fb171b2
william <w3webservices.net@gmail.com>
2023-08-30 11:22:46 -0400

    syncqueue filtered by branchs or mainsite

--- src/services/Message.Services.ts
+++ src/services/Message.Services.ts
@@ -83,8 +83,7 @@ const getClientQueue = async (roomId: string, readID: string) => {
 	const room = await ClientModel.findById(roomId)
 	if(room) {
 		room.toJSON()
-		console.log('read', readID, room._id.toString())
-		if(readID === room._id) {	
+		if(readID === room._id.toString()) {	
 			const queue = await MessageModel.find({ roomId });
 			io.to(roomId).emit("queue", queue);
 		} else {
