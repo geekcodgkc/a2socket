f703d987614a6b9b2f98563baf4db689d66ba9bc
williamjparra <w3webservices.net@gmail.com>
2023-10-18 10:13:12 -0400

    change in the max entity

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ const port = PORT || 3003;
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(
 	morgan((tokens, req, res) => {
