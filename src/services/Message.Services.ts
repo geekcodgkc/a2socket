f2ebeb908448d1a5abe04412ebacb97d274cc8ae
williamjparra <w3webservices.net@gmail.com>
2023-10-31 17:06:31 -0400

    updateDraft function added

--- src/services/Message.Services.ts
+++ src/services/Message.Services.ts
@@ -1,4 +1,5 @@
 import { io } from "../app";
+import { client } from "../store/redis";
 
 /*
 	envia mensajes para actualizar la cola atravez del socket
@@ -12,14 +13,44 @@ const sendMessage = async (
 };
 
 const sendDraft = async (roomId: string) => {
-	io.to(`${roomId}`).emit("draft", { "001": "qty:1" });
+	const draft = await client.get(`draft:${roomId}`);
+
+	io.to(`${roomId}`).emit("draft", draft ? draft : {});
 };
 
-const updateDraft = async (
-	roomId: string,
-	data: string | object | Array<string | object>,
-) => {
-	io.to(`${roomId}`).emit("draft", data);
+interface draftInput {
+	id: string;
+	qty: number;
+}
+
+const updateDraft = async (roomId: string, data: draftInput) => {
+	const draft = await client.get(`draft:${roomId}`);
+	if (draft) {
+		const newDraft = { ...JSON.parse(draft) };
+
+		if (!newDraft[data.id]) {
+			newDraft[data.id] = data.qty;
+		} else {
+			newDraft[data.id] += data.qty;
+		}
+
+		if (newDraft[data.id] < 0) {
+			newDraft[data.id] = 0;
+		}
+
+		const toStore = JSON.stringify(newDraft);
+
+		await client.set(`draft:${roomId}`, toStore, { EX: 3600 });
+
+		io.to(`${roomId}`).emit("draft", newDraft);
+	} else {
+		const newDraft = { [data.id]: data.qty };
+		const toStore = JSON.stringify(newDraft);
+
+		await client.set(`draft:${roomId}`, toStore, { EX: 3600 });
+
+		io.to(`${roomId}`).emit("draft", newDraft);
+	}
 };
 
 export { sendMessage, sendDraft, updateDraft };
